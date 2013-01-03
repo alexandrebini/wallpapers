16a19d336d41ea23a36860c843382503bba5a09b
Alexandre Bini <alexandrebini@gmail.com>
2013-01-03 12:39:44 -0200

    setando o status downloaded"

--- app/workers/wallpaper_download.rb
+++ app/workers/wallpaper_download.rb
@@ -11,20 +11,29 @@ class WallpaperDownload
       filename = File.basename(wallpaper.image_src)
       local_image = Crawler::FileHelper.find_local_image(filename)
       if local_image
+        source = 'local'
         file = File.open(local_image)
       else
+        source = 'remote'
         file = StringIO.new(open_url wallpaper.image_src, proxy: false, min_size: 22.kilobytes)
       end
 
       wallpaper.image = file
       wallpaper.image_file_name = filename
-      wallpaper.save
+      wallpaper.status = 'downloaded'
       wallpaper.colors.destroy_all
 
-      source = local_image ? 'local' : 'remote'
-      download_logger "\nWallpaper #{ wallpaper.id } image: #{ wallpaper.image_src } (#{ source })"
+      if wallpaper.save
+        download_logger "\nWallpaper #{ wallpaper.id } image: #{ wallpaper.image_src } (#{ source })"
+      else
+        raise wallpaper.errors.full_messages.join(' ')
+      end
     rescue Exception => e
-      download_logger "\nError on wallpaper #{ wallpaper.id }: #{ wallpaper.image_src } (#{ source }). #{ e }"
+      if wallpaper
+        download_logger "\nError on wallpaper #{ wallpaper.id }: #{ wallpaper.image_src } (#{ source }). #{ e }"
+      else
+        download_logger "\nError on wallpaper #{ wallpaper_id } (#{ source }). #{ e }"
+      end
       return false
     ensure
       file.close if file
